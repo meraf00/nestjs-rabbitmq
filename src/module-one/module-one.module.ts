b22ac3e390cf3bb838e7850990eb075e6a8d79e8
Leul Wujira <96904152+meraf00@users.noreply.github.com>
2024-06-05 16:28:16 +0300

    feat: add readme

--- src/module-one/module-one.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ModuleOneService } from './module-one.service';
-import { RabbitMQWrapperModule } from 'src/shared/rabbitmq-wrapper/rabbitmq-wrapper.module';
-
-@Module({
-  imports: [RabbitMQWrapperModule],
-  providers: [ModuleOneService],
-})
-export class ModuleOneModule {}
